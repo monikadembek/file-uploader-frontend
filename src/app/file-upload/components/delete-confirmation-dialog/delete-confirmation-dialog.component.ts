8482379acce865b872aa09a293b2d2f753b6cf74
Monika <monika.dembek@gmail.com>
2024-11-28 14:40:08 +0100

    Add confirmation dialog before deleting file

---
+++ src/app/file-upload/components/delete-confirmation-dialog/delete-confirmation-dialog.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-delete-confirmation-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './delete-confirmation-dialog.component.html',
+  styleUrl: './delete-confirmation-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DeleteConfirmationDialogComponent {
+
+}
